dd8d3144958b31b7afadc4d7f3b64904f57b3cb2
Mateo Tabernero <51258651+Mateotabernero@users.noreply.github.com>
2023-12-31 14:28:36 +0100

    Update newtonRaphson.py

--- newton/newtonRaphson.py
+++ newton/newtonRaphson.py
@@ -1,1 +1,14 @@
+def newtonRaphson(f, fder, x0, maxIter = 1000, tol = 10**(-2)): 
+    x = x0 
+    n = 0
+    lastError = tol +1 
+    while(lastError > tol and n < maxIter):
+        ### Hay que introducir mensajes de error si fder(x0) no está definido 
+        x -= f(x)/fder(x0)
+
+        lastError = abs(f(x))
+
+        n += 1 
+
+    return x
 
